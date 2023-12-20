045f4fc40b1467735e312dd6629ba355c77b7b1a
amarnathdp <118151760+amarnathdp@users.noreply.github.com>
2023-12-20 19:31:49 +0530

    Update whatsAppSender.js

--- src/lib/whatsAppSender.js
+++ src/lib/whatsAppSender.js
@@ -82,11 +82,12 @@ class WhatsAppSender {
                 },
                 data: message
             };
-            await axios(config).then( response => {
-                self.eventsEmitter.emit(Config.EVENT_WHATSAPP_MESSAGE_DELIVERED, response.data.messages[0].id);
-            }).catch(function (error) {
-                throw new Error(error);
-            });
+            await axios(config)
+            //     .then( response => {
+            //     self.eventsEmitter.emit(Config.EVENT_WHATSAPP_MESSAGE_DELIVERED, response.data.messages[0].id);
+            // }).catch(function (error) {
+            //     throw new Error(error);
+            // });
         } catch (error) {
             throw error;
         }
